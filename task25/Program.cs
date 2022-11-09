71a1e751f68b7e76d51085ace1842d6e935fdd50
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-09 20:30:38 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int PowerArray(int a, int b)
+{
+    int result = 1; 
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int powerArray = PowerArray(a , b);
+Console.WriteLine($"Чило {a} в степени {3} равно " + powerArray);
+
